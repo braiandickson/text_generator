96e191bb9cc06463864bedcba3d3b09cd7b375fe
Braian Dickson <braiandickson@gmail.com>
2017-05-04 15:55:25 -0300

    Adds the code of the app.

--- text_generator/src/App.js
+++ text_generator/src/App.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Output from './Components/Output';
+import Select from './Components/Controls/Select';
+import Text from './Components/Controls/Text';
+import axios from 'axios';
 
 class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      paras: 4,
+      html: true,
+      text: ''
+    }
+  }
+
+  componentWillMount(){
+    this.getSampleText();
+  }
+
+  getSampleText(){
+    axios.get('http://hipsterjesus.com/api/?='+this.state.paras+'&html='+this.state.html)
+    .then((response) => {
+      this.setState({text: response.data.text}, function(){
+        console.log(this.state);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      });
+    };
+
+  showHtml(x){
+    this.setState({html: x}, this.getSampleText);
+  }
+
+  changeParas(number){
+    this.setState({paras: number}, this.getSampleText);
+  }  
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <h1 className="text-center">ReactJS Sample Text Generator</h1>
+        <hr />
+        <form className="form-inline">
+          <div className="form-group">
+            <label>Paragraphs:</label>
+            <Text value={this.state.paras} onChange={this.changeParas.bind(this)} />
+          </div>
+          <div className="form-group">
+            <label>Include HTML:</label>
+            <Select value={this.state.html} onChange={this.showHtml.bind(this)} />
+          </div>
+        </form>
+        <br /><br />
+        <Output value={this.state.text} />
       </div>
     );
   }
